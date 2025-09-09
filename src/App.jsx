a41ed2b4908dbee8ef447ed3a8197de1ff1291d0
kmaclip <kmartclips@proton.me>
2025-09-09 07:16:46 -0400

    Add main App component with routing structure

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
+import BlueprintCreator from './components/BlueprintCreator'
+import Login from './components/Login'
+import Register from './components/Register'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <BlueprintCreator />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </Router>
+  )
+}
+
+export default App
